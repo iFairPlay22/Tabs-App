cc7f0a77bdd218e4900f2702c0ed947155cafe68
iFairPlay22 <ewen.bouquet@gmail.com>
2021-05-02 09:52:30 +0200

    Add /index.php in prod env

--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,12 @@ const $ = require("jquery");
 require("bootstrap");
 
 import "./styles/app.scss";
+
+let url = document.URL;
+
+// Ajout du /index.php en prod
+let domain = ".herokuapp.com";
+if (url.includes(domain) && !url.includes("/index.php")) {
+    let data = url.split(domain);
+    document.location = data[0] + `${domain}/index.php` + data[1];
+}
